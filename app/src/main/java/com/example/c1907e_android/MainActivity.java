b691c90ccce8086ca90b9224741947b939af3a9e
duyleomessi <duyleitbka95@gmail.com>
2021-07-19 19:17:01 +0700

    add 2 intent

--- app/src/main/java/com/example/c1907e_android/MainActivity.java
+++ app/src/main/java/com/example/c1907e_android/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.c1907e_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Button nextButton = (Button) findViewById(R.id.click_me);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
