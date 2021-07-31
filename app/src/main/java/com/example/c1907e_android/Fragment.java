17658fb013cc8a7eed5972cf89efbcd2854f3042
duyleomessi <duyleitbka95@gmail.com>
2021-07-31 08:48:36 +0700

    fragment

---
+++ app/src/main/java/com/example/c1907e_android/Fragment.java
@@ -0,0 +1,14 @@
+package com.example.c1907e_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
